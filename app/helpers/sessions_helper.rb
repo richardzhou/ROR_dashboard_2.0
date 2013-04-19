fe2edae13f2c48b62ee5e40c7991f00da94968ea
Richard <richard@rIchard-VirtualBox.(none)>
2013-04-19 18:43:00 -0400

    daily_push

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
   def show_div_if(condition, attributes={}, &block)
     unless condition
-      attributes["style"]="visibility:visible"
+      attributes[:style]="visibility:visible"
     end
     content_tag("div",attributes,&block)
   end
